60afa60d215f862f3286d2a1979c512c6f39b7c3
Egor <egor951769794@gmail.com>
2024-05-22 17:29:28 +0600

    регистрация + авторизация, доработки окна авторизации

--- server.ts
+++ server.ts
@@ -1,10 +1,7 @@
-// const express = require('express')
-
 import express from 'express'
-
-
 import { User } from './src/data/Users'
 import { Request, Response } from 'express'
+import { hash, compare } from 'bcrypt'
 
 const app = express()
 app.use(express.json())
@@ -33,12 +30,30 @@ app.get('/users', (req: Request, res: Response) => {
     res.json(users)
 })
 
-app.post('/users', (req: Request, res: Response) => {
-    console.log(req.body)
-    const user : User = { login: req.body.login, password: req.body.password, cart: [] }
-    users.push(user)
+app.post('/users', async (req: Request, res: Response) => {
+    if (users.find(user => user.login === req.body.login)) return res.status(400).send('Такой пользователь уже существует')
+    try {
+        const hashPassword = await hash(req.body.password, 8)
+        const user : User = { login: req.body.login, password: hashPassword, cart: [] }
+        users.push(user)
+    } catch(err) {
+        res.status(500).send()
+        console.log(err)
+    }
+
     res.status(201).send()
 })
 
+app.post('/users/login', async(req: Request, res: Response) => {
+    const user = users.find(user => user.login === req.body.login)
+    if (user == null) return res.status(400).send('Неверный логин')
+    try {
+        if (await compare(req.body.password, user.password)) res.send(true)
+        else res.send(false)
+    } catch(err) {
+        res.status(500).send()
+        console.log(err)
+    }
+})
 
 app.listen(3001)
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,8 +3,7 @@ import './Login.css';
 
 import axios from 'src/utils/axios'
 const reg_url = '/users'
-
-
+const auth_url = 'users/login'
 
 
 type LoginProps = {
@@ -32,57 +31,87 @@ export default function Login(props: LoginProps) {
     }
   )
 
+  const [regDisabled, setRegDisabled] = useState(true)
+  const [authDisabled, setAuthDisabled] = useState(true)
+
   useEffect(() => {
     setErrMsg('')
   }, [registrationData, authData])
 
+  useEffect(() => {
+    setRegistrationData({login: '', password: ''})
+    setAuthData({login: '', password: ''})
+    setAuthDisabled(true)
+    setRegDisabled(true)
+  }, [windowState])
 
-  const handleReg = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRegistrationData({...registrationData, [event.target.name] : event.target.value})
+  const handleRegLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRegistrationData({...registrationData, login : event.target.value})
+    setRegDisabled(event.target.value.length < 5 || registrationData.password.length < 5)
   }
 
-  const handleAuth = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuthData({...authData, [event.target.name] : event.target.value})
+  const handleRegPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRegistrationData({...registrationData, password : event.target.value})
+    setRegDisabled(event.target.value.length < 5 || registrationData.login.length < 5)
+  }
+
+  const handleAuthLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuthData({...authData, login : event.target.value})
+    setAuthDisabled(event.target.value.length < 5 || authData.password.length < 5)
+  }
+
+  const handleAuthPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuthData({...authData, password : event.target.value})
+    setAuthDisabled(event.target.value.length < 5 || authData.login.length < 5)
   }
 
   const handleRegSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
-    try {
-      console.log(registrationData)
-      const response = await axios.post(reg_url,
-        {"login": registrationData.login, "password": registrationData.password},
-        {
-          headers: { 'Content-Type' : 'application/json' },
-          withCredentials: true
-        }
-      )
-    } catch(err) {
-      console.log(err)
-      console.log(registrationData)
-    }
+    await axios.post(reg_url,
+      registrationData,
+      {
+        headers: { 'Content-Type' : 'application/json' },
+        withCredentials: true
+      }
+    ).then((response) => {
+      setRegistrationData({login: '', password: ''})
+      setErrMsg('Пользователь успешно создан')
+    })
+    .catch((error) => {console.log(error)})
   }
 
-  const handleAuthSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleAuthSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
+    console.log(authData)
+
+    await axios.post(auth_url,
+      authData,
+      {
+        headers: { 'Content-Type' : 'application/json' },
+        withCredentials: true
+      }
+    ).then((response) => {setSuccess(response.data)})
+    .catch((error) => setErrMsg(error.response.data))
   }
 
   return (
     <div className='login-container'>
       {windowState ?
       <form className='login-form' onSubmit={handleRegSubmit}>
-        <input name='login' type='text' placeholder='Адрес эл.почты' value={registrationData.login} onChange={handleReg}/>
-        <input name='password' type='password' placeholder='Пароль' value={registrationData.password} onChange={handleReg}/>
-        <button type='submit' className='login-button'>Зарегистрироваться</button>
+        <input name='login' type='text' placeholder='Адрес эл.почты' value={registrationData.login} onChange={handleRegLogin}/>
+        <input name='password' type='password' placeholder='Пароль' value={registrationData.password} onChange={handleRegPassword}/>
+        <button disabled={regDisabled} type='submit' className={regDisabled? 'login-button disabled' : 'login-button'}>Зарегистрироваться</button>
       </form>
       : 
       <form className='login-form' onSubmit={handleAuthSubmit}>
-        <input name='login' type='text' placeholder='Адрес эл.почты' value={authData.login} onChange={handleAuth}/>
-        <input name='password' type='password' placeholder='Пароль' value={authData.password} onChange={handleAuth}/>
-        <button type='submit' className='login-button'>Войти</button>
+        <input name='login' type='text' placeholder='Логин' value={authData.login} onChange={handleAuthLogin}/>
+        <input name='password' type='password' placeholder='Пароль' value={authData.password} onChange={handleAuthPassword}/>
+        <button disabled={authDisabled} type='submit' className={authDisabled? 'login-button disabled' : 'login-button'}>Войти</button>
       </form>}
       <div className='login-switch'>
+        <div>{errMsg}</div>
         <div className={'login-switch-option'.concat(windowState? '' : ' selected')} onClick={() => setWindowState(0)}>Вход</div>
         <div className={'login-switch-option'.concat(windowState? ' selected' : '')} onClick={() => setWindowState(1)}>Регистрация</div>
       </div>
